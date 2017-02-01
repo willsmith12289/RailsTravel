1e6c1fbc88655fb6622ad05b98b0feeba1b69e71
will <willsmith12289@gmail.com>
2017-02-01 17:00:17 -0500

    added personal info

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,146 +1,172 @@
-	 var markers = [];
+var markers = [];
 
-	 function initialize() {
+function initialize() {
 
-	 	var myOptions = {
-	 		mapTypeId: google.maps.MapTypeId.ROADMAP,
-	 		center: {
-	 			lat: gon.lat,
-	 			lng: gon.lng
-	 		},
-	 		zoom: 12,
-	 		scrollwheel: false,
-	 		draggable: true,
-	 	};
+	var myOptions = {
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		center: {
+			lat: gon.lat,
+			lng: gon.lng
+		},
+		zoom: 12,
+		scrollwheel: false,
+		draggable: true,
+	};
 
-	 	var map = new google.maps.Map(document.getElementById('map'), myOptions);
+	var map = new google.maps.Map(document.getElementById('map'), myOptions);
 
-	 	//auto complete / bias autocomplete to current maps bounds
-	 	var addressInput = new google.maps.places.SearchBox(document.getElementById('marker_raw_address'));
-	 	var form = document.getElementById('form');
-	 	form.addEventListener("submit", autoComplete);
-	 	var placeInput = document.getElementById('marker_place_id');
-	 	map.addListener('bounds_changed', function() {
-	 		addressInput.setBounds(map.getBounds());
-	 	});
-
-
-	 	/*
-	 	 *  when address in autocomplete is selected place_id is posted to Marker
-	 	 */
-	 	addressInput.addListener('places_changed', function() {
-	 		var places = addressInput.getPlaces();
-	 		places.forEach(function(place) {
-	 			var placeId = place.place_id;
-	 			placeInput.value = placeId;
-	 		})
-	 	});
+	//auto complete / bias autocomplete to current maps bounds
+	var addressInput = new google.maps.places.SearchBox(document.getElementById('marker_raw_address'));
+	var form = document.getElementById('form');
+	form.addEventListener("submit", autoComplete);
+	var placeInput = document.getElementById('marker_place_id');
+	map.addListener('bounds_changed', function() {
+		addressInput.setBounds(map.getBounds());
+	});
 
 
-	 	/*
-	 	 * grab correct place_id and info from marker model
-	 	 */
-	 	document.addEventListener("DOMContentLoaded", function() {
-	 		var tRows = document.getElementsByTagName('tr');
-	 		for (var i = 0; i < tRows.length; i++) {
-	 			var placeId = tRows[i].cells[6].textContent;
-	 			placeId = placeId.toString();
-	 			placeId = placeId.trim();
-	 			getPlaceFromId(placeId);
-	 		};
-	 	});
+	/*
+	 *  when address in autocomplete is selected place_id is posted to Marker
+	 */
+	addressInput.addListener('places_changed', function() {
+		var places = addressInput.getPlaces();
+		places.forEach(function(place) {
+			var placeId = place.place_id;
+			placeInput.value = placeId;
+		})
+	});
 
 
-	 	/*
-	 	 * Make place service request on place_id passed from dom event listener
-	 	 */
-	 	function getPlaceFromId(place) {
-	 		var request = {
-	 			placeId: place
-	 		};
-	 		var service = new google.maps.places.PlacesService(map);
-	 		service.getDetails(request, callback);
-
-	 		function callback(place, status) {
-	 			if (status == google.maps.places.PlacesServiceStatus.OK) {
-	 				addMarker(place);
-	 			}
-	 		}
-	 	};
+	/*
+	 * grab correct place_id and info from marker model
+	 */
+	document.addEventListener("DOMContentLoaded", function() {
+		var tRows = document.getElementsByTagName('tr');
+		for (var i = 0; i < tRows.length; i++) {
+			var placeId = tRows[i].cells[6].textContent;
+			placeId = placeId.toString();
+			placeId = placeId.trim();
+			getPlaceFromId(placeId);
+		};
+	});
 
 
-	 	/*
-	 	 * get place on form submit and call createMarker on place
-	 	 */
-	 	function autoComplete() {
-	 		var Gplace = addressInput.getPlaces();
+	/*
+	 * Make place service request on place_id passed from dom event listener
+	 */
+	function getPlaceFromId(place) {
+		var request = {
+			placeId: place
+		};
+		var service = new google.maps.places.PlacesService(map);
+		service.getDetails(request, callback);
 
-	 		// For each place, get the icon, name and location.
-	 		var bounds = new google.maps.LatLngBounds();
-	 		Gplace.forEach(function(place) {
-	 			if (!place.geometry) {
-	 				console.log("Returned place contains no geometry");
-	 				return;
-	 			}
-	 			// Create a marker for each place.
-	 			addMarker(place);
-	 			if (place.geometry.viewport) {
-	 				// Only geocodes have viewport.
-	 				bounds.union(place.geometry.viewport);
-	 			} else {
-	 				bounds.extend(place.geometry.location);
-	 			}
-	 			var placeId = place.place_id;
-	 		});
-	 		map.fitBounds(bounds);
-	 	};
+		function callback(place, status) {
+			if (status == google.maps.places.PlacesServiceStatus.OK) {
+				addMarker(place);
+			}
+		}
+	};
 
 
-	 	/*
-	 	 * assigns icon, creates marker/infowindow, populates w/ getDetails request
-	 	 * Pushes marker to markers array for use in marker clusterer
-	 	 */
+	/*
+	 * get place on form submit and call createMarker on place
+	 */
+	function autoComplete() {
+		var Gplace = addressInput.getPlaces();
 
-	 	function addMarker(place) {
-	 		var icon = {
-	 			url: place.icon,
-	 			size: new google.maps.Size(71, 71),
-	 			origin: new google.maps.Point(0, 0),
-	 			anchor: new google.maps.Point(17, 34),
-	 			scaledSize: new google.maps.Size(25, 25)
-	 		};
-	 		var marker = new google.maps.Marker({
-	 			map: map,
-	 			icon: icon,
-	 			title: place.name,
-	 			position: place.geometry.location
-	 		});
-	 		markers.push(marker);
-	 		marker.infowindow = new google.maps.InfoWindow();
-
-	 		//console.log(place);
-	 		var service = new google.maps.places.PlacesService(map);
-
-	 		service.getDetails({
-	 			placeId: place.place_id
-	 		}, function(place, status) {
-	 			if (status === google.maps.places.PlacesServiceStatus.OK) {
-	 				google.maps.event.addListener(marker, 'click', formatInfoWindow.bind(marker, place));
-	 			};
-	 		});
-	 	};
-
-	 	function formatInfoWindow(place) {
-	 		this.infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-	 			'Place ID: ' + place.place_id + '<br>' +
-	 			this.formatted_address + '</div><br><p>' + place.formatted_phone_number + '</p>');
-	 			this.infowindow.open(map, this);
-	 	}
-	 	window.onload = function() {
-	 		var markerCluster = new MarkerClusterer(map, markers, {
-	 			imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-	 		});
-	 	};
+		// For each place, get the icon, name and location.
+		var bounds = new google.maps.LatLngBounds();
+		Gplace.forEach(function(place) {
+			if (!place.geometry) {
+				console.log("Returned place contains no geometry");
+				return;
+			}
+			// Create a marker for each place.
+			addMarker(place);
+			if (place.geometry.viewport) {
+				// Only geocodes have viewport.
+				bounds.union(place.geometry.viewport);
+			} else {
+				bounds.extend(place.geometry.location);
+			}
+			var placeId = place.place_id;
+		});
+		map.fitBounds(bounds);
+	};
 
 
-	 }
+	/*
+	 * assigns icon, creates marker, makes getDetails request and calls
+	 * formatInfoWindow() Pushes marker to markers array for use in      * marker clusterer
+	 */
+
+	function addMarker(place) {
+		var icon = {
+			url: place.icon,
+			size: new google.maps.Size(71, 71),
+			origin: new google.maps.Point(0, 0),
+			anchor: new google.maps.Point(17, 34),
+			scaledSize: new google.maps.Size(25, 25)
+		};
+		var marker = new google.maps.Marker({
+			map: map,
+			icon: icon,
+			title: place.name,
+			position: place.geometry.location
+		});
+		markers.push(marker);
+		marker.infowindow = new google.maps.InfoWindow();
+		var infos = gon.info;
+		marker.info;
+				for (var i = 0; i < markers.length; i++) {
+					marker.info = infos[i];
+				}
+		//console.log(place);
+		var service = new google.maps.places.PlacesService(map);
+
+		service.getDetails({
+			placeId: place.place_id
+		}, function(place, status) {
+			if (status === google.maps.places.PlacesServiceStatus.OK) {
+				google.maps.event.addListener(marker, 'click', formatInfoWindow.bind(marker, place));
+			};
+		});
+	};
+
+
+/*
+* Gets returned details and assigns them to infowindow content
+*/
+	function formatInfoWindow(place) {
+		var name = place.name,
+				open = place.opening_hours.open_now,
+				address = place.formatted_address,
+				phoneI = place.international_phone_number,
+				phone = place.formatted_phone_number
+				rating = place.rating,
+				reviewAuth = place.reviews[0].author_name,
+				reviewText = place.reviews[0].text,
+				reviewRate = place.reviews[0].rating,
+				website = place.website,
+				info = this.info;
+		this.infowindow.setContent(
+'<div class="infowindow"><strong><h1>' + name + '</h1></strong>' +
+	'<p>Open Now: ' + open + '</p>'+
+	'<address>' +	address + '</address>'+
+	'<p><a href="' + website + '">Website</a></p>'+
+	'<p><a href="tel:' + phoneI + '">'+phone+'</a></p>'+
+	'<p>Rating: ' + rating + '</p>'+
+	'<h4>' + reviewAuth + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rating: '+reviewRate+'</h4>'+
+	'<p>' + reviewText + '</p>'+
+	'<p>' + info + '</p>'+
+'</div>'
+		);
+		this.infowindow.open(map, this);
+	}
+	window.onload = function() {
+		var markerCluster = new MarkerClusterer(map, markers, {
+			imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+		});
+	};
+}
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -13,9 +13,7 @@ class MapsController < ApplicationController
     gon.lng = @map.longitude
     @marker = Marker.new
     @markers = Marker.where(map_id: @map)
-    gon.lat_lng_array = Map.coordinates(@map)
     gon.info = Map.info(@map)
-    gon.path = map_markers_url(@map)
   end
 
   def create
